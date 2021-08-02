965343f9ced03660757b82390f05133cfa8af45a
Jordan <mahsman27@gmail.com>
2021-08-01 23:39:14 -0500

    added some logic, as well as some css and html changes.

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-randomNum => {
-    return Math.floor(Math.random() * 10000 ) + 1;
-    console.log('hit randonnum')
+//generate number between 1 - 10
+generateRandomNum = () => {
+    return Math.floor((Math.random() * 10) + 1);     
 }
 
+//get all of the buttons in the html
+let buttons = document.querySelectorAll('button');
 
+//simple for loop that uses the generateRandomNum function to insert a number into the confines of the button
+for (i of buttons) {
+  i.innerText = generateRandomNum();
+}
 
+//need each value in the buttons to be unique
+//one easy way would be to change math random from 10 to 100 or 1000.. still a chance they will be the same.. but less of one :)
+
+// 2. add event listener for click of the buttons
+// 3. determine 'correct' number
+// 4. check user input versus 'correct' number
+// 5. add to the score at the bottom, if correct
